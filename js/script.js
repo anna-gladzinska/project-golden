5ec973629e15eee9d7e1363deebe84d81a397eac
Anna Gladzińska <agladzinska@gmail.com>
2019-10-28 21:48:17 +0100

    Add nav + header

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var elMenu = document.querySelector('.menu');
+var elToggle = document.querySelector('.menu-toggle');
+elToggle.addEventListener('click', function() {
+elMenu.classList.toggle('open');
+})
